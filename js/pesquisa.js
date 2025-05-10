978570f10bc3c8b5e0bbe9a4985bfd09046d86ee
Marcos de Oliveira Brandão Junior <marcosde.o.b.junior@gmail.com>
2025-05-10 08:46:10 -0300

    Update pesquisa.js

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,1 +1,13 @@
+// Salva o termo de pesquisa e redireciona para pesquisa.html
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.getElementById('searchInput');
+  if (searchInput) {
+    searchInput.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        localStorage.setItem('termoPesquisa', searchInput.value);
+        window.location.href = 'pesquisa.html';
+      }
+    });
+  }
+});
 
